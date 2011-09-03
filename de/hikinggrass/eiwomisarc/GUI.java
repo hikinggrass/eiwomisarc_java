7d28abf79bcfefb7ebcfa4cdf8594a09c7c09314
Kai Hermann <kai.uwe.hermann@gmail.com>
2011-09-03 19:30:51 +0200

    tweaked the gui a little bit
    Signed-off-by: Kai Hermann <kai.uwe.hermann@gmail.com>

--- de/hikinggrass/eiwomisarc/GUI.java
+++ de/hikinggrass/eiwomisarc/GUI.java
@@ -9,6 +9,8 @@ import javax.swing.JButton;
 import javax.swing.JSlider;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GUI {
 
@@ -90,7 +92,7 @@ public class GUI {
 		frame.getContentPane().setLayout(null);
 
 		JButton btnInitialisierung = new JButton("Initialisierung");
-		btnInitialisierung.setBounds(18, 37, 131, 29);
+		btnInitialisierung.setBounds(6, 37, 145, 29);
 		btnInitialisierung.addActionListener(new ActionListener() {
 
 			@Override
@@ -101,34 +103,46 @@ public class GUI {
 		});
 		frame.getContentPane().add(btnInitialisierung);
 
-		JButton btnGlobaleFarbe = new JButton("Globale Farbe");
-		btnGlobaleFarbe.setBounds(7, 78, 117, 29);
-		btnGlobaleFarbe.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				globalColor();
-			}
-		});
-		frame.getContentPane().add(btnGlobaleFarbe);
-
 		sliderR = new JSlider();
 		sliderR.setValue(0);
 		sliderR.setMaximum(127);
 		sliderR.setBounds(146, 78, 190, 29);
+		sliderR.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				globalColor();
+			}
+		});
 		frame.getContentPane().add(sliderR);
 
 		sliderG = new JSlider();
 		sliderG.setMaximum(127);
 		sliderG.setValue(0);
 		sliderG.setBounds(146, 119, 190, 29);
+		sliderG.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				globalColor();
+			}
+		});
 		frame.getContentPane().add(sliderG);
 
 		sliderB = new JSlider();
 		sliderB.setValue(0);
 		sliderB.setMaximum(127);
 		sliderB.setBounds(146, 160, 190, 29);
+		sliderB.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				globalColor();
+			}
+		});
 		frame.getContentPane().add(sliderB);
 
 		JLabel lblR = new JLabel("R");
@@ -143,55 +157,59 @@ public class GUI {
 		lblB.setBounds(136, 158, 15, 16);
 		frame.getContentPane().add(lblB);
 
-		JButton btnLauflicht = new JButton("Lauflicht");
-		btnLauflicht.setBounds(7, 197, 92, 29);
-		btnLauflicht.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				lauflicht();
-			}
-		});
-		frame.getContentPane().add(btnLauflicht);
-
 		sliderSpeed = new JSlider();
 		sliderSpeed.setMaximum(255);
 		sliderSpeed.setMinimum(1);
 		sliderSpeed.setValue(1);
 		sliderSpeed.setBounds(146, 197, 190, 29);
+		sliderSpeed.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				lauflicht();
+			}
+		});
 		frame.getContentPane().add(sliderSpeed);
 
 		JLabel lblSpeed = new JLabel("Speed");
 		lblSpeed.setBounds(111, 201, 48, 16);
 		frame.getContentPane().add(lblSpeed);
 
-		textSerial = new JTextField("test");
-		textSerial.setBounds(153, 36, 92, 28);
+		textSerial = new JTextField("/dev/ttyUSB0");
+		textSerial.setBounds(153, 36, 110, 28);
 		frame.getContentPane().add(textSerial);
 		textSerial.setColumns(10);
 
 		textBaud = new JTextField("9600");
-		textBaud.setBounds(248, 36, 61, 28);
+		textBaud.setBounds(275, 36, 61, 28);
 		frame.getContentPane().add(textBaud);
 		textBaud.setColumns(10);
 
 		JLabel lblNewLabel = new JLabel("Serial Port");
-		lblNewLabel.setBounds(153, 8, 77, 16);
+		lblNewLabel.setBounds(174, 8, 77, 16);
 		frame.getContentPane().add(lblNewLabel);
 
 		JLabel lblBaudrate = new JLabel("Baudrate");
-		lblBaudrate.setBounds(248, 8, 61, 16);
+		lblBaudrate.setBounds(275, 8, 61, 16);
 		frame.getContentPane().add(lblBaudrate);
 
 		textNumberOfLEDStripes = new JTextField();
 		textNumberOfLEDStripes.setText("1");
-		textNumberOfLEDStripes.setBounds(323, 36, 61, 28);
+		textNumberOfLEDStripes.setBounds(354, 36, 61, 28);
 		frame.getContentPane().add(textNumberOfLEDStripes);
 		textNumberOfLEDStripes.setColumns(10);
 
 		JLabel lblAnzahlLeisten = new JLabel("Anzahl Leisten");
-		lblAnzahlLeisten.setBounds(323, 8, 100, 16);
+		lblAnzahlLeisten.setBounds(344, 8, 100, 16);
 		frame.getContentPane().add(lblAnzahlLeisten);
+		
+		JLabel lblGlobaleFarbe = new JLabel("Globale Farbe:");
+		lblGlobaleFarbe.setBounds(18, 119, 95, 16);
+		frame.getContentPane().add(lblGlobaleFarbe);
+		
+		JLabel lblLauflicht = new JLabel("Lauflicht:");
+		lblLauflicht.setBounds(18, 197, 61, 16);
+		frame.getContentPane().add(lblLauflicht);
 	}
 }
